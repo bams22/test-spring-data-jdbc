390a63c83865a380b0656f91a0a6ac10a2585e33
Панасенко Сергей Евгеньевич <s.panasenko@cdek.ru>
2023-08-12 14:49:40 +0700

    Initial commit

---
+++ src/main/java/com/bams22/test/spring/data/jdbc/TestSpringDataJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.bams22.test.spring.data.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringDataJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringDataJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bams22/test/spring/data/jdbc/TestSpringDataJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bams22.test.spring.data.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringDataJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
